76a4c30f1a14dfd1e726d242ac1d2fde5ba6c962
Agus Ruscio <99211308+AgustinRuscio@users.noreply.github.com>
2023-09-21 01:01:08 -0300

    Quality settings added. Credits and How to play added

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,41 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    
+    public void BTN_Play(string Scene) => SceneManager.LoadScene(Scene);        
 
+    public void BTN_Exit() => Application.Quit();
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-
-    public void ButtonPlayClicked(string Scene)
-    {
-        SceneManager.LoadScene(Scene);        
-
-    }
-
-    public void ButtonExitClicked()
-    {
-
-        Application.Quit();
-    }
-
-
-
+  
 }
