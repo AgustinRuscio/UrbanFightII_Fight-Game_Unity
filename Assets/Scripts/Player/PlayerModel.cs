16e84903a1e9daac67e8141947eb7118267d70bd
Meme <MemeRiego@users.noreply.github.com>
2023-09-26 21:05:54 -0300

    Public Life

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -33,8 +33,8 @@ public class PlayerModel : NetworkBehaviour, IDamageable
     [SerializeField]
     private float _maxLlife;
 
-    [Networked(OnChanged = nameof(OnLifeChange))]
-    float _life { get; set; }
+    [Networked(OnChanged = nameof(OnLifeChange))] [HideInInspector]
+    public float _life { get; set; }
 
     public event Action<float> OnDamage = delegate { };
 
